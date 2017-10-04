256a11782d2c1616e7e58407603e168cb0cc1fba
Charleston Anjos <charleston.anjos@gmail.com>
2017-10-04 00:22:19 -0300

    ajustes de configuração e layout

--- android/AndroidKotlinProject2/app/src/main/java/charleston/androidkotlinproject/features/proprierties/apresentations/PropriertActivity.kt
+++ android/AndroidKotlinProject2/app/src/main/java/charleston/androidkotlinproject/features/proprierties/apresentations/PropriertActivity.kt
@@ -2,6 +2,7 @@ package charleston.androidkotlinproject.features.proprierties.apresentations
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import charleston.androidkotlinproject.R
 import charleston.androidkotlinproject.data.domain.Propriertie
 import charleston.androidkotlinproject.features.proprierties.presenters.PropriertiePresenter
 import charleston.androidkotlinproject.features.proprierties.presenters.PropriertieView
@@ -16,6 +17,7 @@ class PropriertActivity : AppCompatActivity(), PropriertieView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
         presenter.findAll()
     }
